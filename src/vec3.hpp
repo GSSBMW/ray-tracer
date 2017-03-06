a3f7c3ae763e7919f2eab4e7d6334f90c65d77ec
gss <1294972664@qq.com>
2017-03-06 15:57:54 +0800

    Tab to Space.

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -5,23 +5,23 @@ template <typename T>
 class Vec3
 {
 public:
-	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-	Vec3(const T &xx) : x(xx), y(xx), z(xx) {}
-	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
-	
-	double length();
-	Vec3<T> normalize();
-	
-	T dot(const Vec3<T> &v) const;
-	Vec3<T> cross(const Vec3<T> &v) const;
-	
-	Vec3<T> operator + (const Vec3<T> &v) const;
-	Vec3<T> operator - (const Vec3<T> &v) const;
-	Vec3<T> operator * (const T &r) const;
-	
-	T x, y, z;
+    Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+    Vec3(const T &xx) : x(xx), y(xx), z(xx) {}
+    Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+    
+    double length();
+    Vec3<T> normalize();
+    
+    T dot(const Vec3<T> &v) const;
+    Vec3<T> cross(const Vec3<T> &v) const;
+    
+    Vec3<T> operator + (const Vec3<T> &v) const;
+    Vec3<T> operator - (const Vec3<T> &v) const;
+    Vec3<T> operator * (const T &r) const;
+    
+    T x, y, z;
 };
 
 typedef Vec3<float> Vec3f;
 
-#endif	// RT_VEC3_HPP_
+#endif  // RT_VEC3_HPP_
