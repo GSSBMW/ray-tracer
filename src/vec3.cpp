0e97b261e4adb5fc0677123889d1825f97b0ae15
gss <1294972664@qq.com>
2017-03-06 15:52:34 +0800

    Refine Vec3 class.

---
+++ src/vec3.cpp
@@ -0,0 +1,47 @@
+#include "vec3.hpp"
+
+float Vec3<T>::length()
+{
+	return sqrt(x * x + y * y + z * z);
+}
+
+Vec3<T>& Vec3<T>::normalize()
+{
+	float len = length();
+	if (len > 0)
+	{
+		float invlen = 1.0 / len;
+		x *= invlen;
+		y *= invlen;
+		z *= invlen;
+	}
+	
+	return *this;
+}
+
+T Vec3<T>::dot(const Vec3<T> &v) const
+{
+	return x * v.x + y * v.y + z * v.z;
+}
+
+Vec3<T> Vec3<T>::cross(const Vec3<T> &v) const
+{
+	return Vec3<T>( y * v.z - z * v.y,
+					z * v.x - x * v.z,
+					x * v.y - y * v.x );
+}
+
+Vec3<T> Vec3<T>::operator + (const Vec3<T> &v) const
+{
+	return Vec3<T>(x + v.x, y + v.y, z + v.z); 
+}
+
+Vec3<T> Vec3<T>::operator - (const Vec3<T> &v) const
+{
+	return Vec3<T>(x - v.x, y - v.y, z - v.z);
+}
+
+Vec3<T> Vec3<T>::operator * (const T &r) const
+{
+	return Vec3<T>(x * r, y * r, z * r);
+}
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -1,5 +1,6 @@
 #ifndef RT_VEC3_HPP_
 #define RT_VEC3_HPP_
+
 template <typename T>
 class Vec3
 {
@@ -8,7 +9,16 @@ public:
 	Vec3(const T &xx) : x(xx), y(xx), z(xx) {}
 	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
 	
-private:
+	double length();
+	Vec3<T> normalize();
+	
+	T dot(const Vec3<T> &v) const;
+	Vec3<T> cross(const Vec3<T> &v) const;
+	
+	Vec3<T> operator + (const Vec3<T> &v) const;
+	Vec3<T> operator - (const Vec3<T> &v) const;
+	Vec3<T> operator * (const T &r) const;
+	
 	T x, y, z;
 };
 
